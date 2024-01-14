4aca516d2ba6c99c1fda3f37d5a8fff836fcf90e
Ramon Domingos <ramongemio@gmail.com>
2024-01-14 19:20:20 -0300

    add tests with jMeter

--- api/src/logic/database.ts
+++ api/src/logic/database.ts
@@ -1,25 +1,31 @@
-import { createConnection} from 'mysql';
+import { createConnection } from 'mysql';
 
 import AppConfig from '../config/configs';
 
-export async function makeQuery(query:string) {
-   
-    var con = createConnection({
-        host: AppConfig.MYSQL_HOST,
-        user: 'user',
-        password: '123456Aa@', 
-        port: 3306
-      });
-      return new Promise((resolve, reject) => {
-        con.connect(async function(err) {
-          if (err) throw err ;
-           await con.query(query, async function (err, result, fields) {
-            if (err) throw err;
-          
-            return   resolve(JSON.parse(JSON.stringify(result)) )
-          });
+export async function makeQuery(query: string) {
+
+  var con = createConnection({
+    host: AppConfig.MYSQL_HOST,
+    user: 'user',
+    password: '123456Aa@',
+    port: 3306
+  });
+  return new Promise((resolve, reject) => {
+    con.connect(async function (err) {
+      if (err) throw err;
+      await con.query(query, async function (err, result, fields) {
+        if (err) throw err;
+        con.end((errorr) => {
+          if (errorr) {
+            console.error('Error closing MySQL connection:', errorr);
+            return;
+          }
+          console.log('MySQL connection closed.');
         });
+        return resolve(JSON.parse(JSON.stringify(result)))
       });
-       
+    });
+  });
+
 
 }
--- uniPot/src/auth.ts
+++ uniPot/src/auth.ts
@@ -1,40 +1,40 @@
-import { sign, SignOptions, verify  } from 'jsonwebtoken';
+import { sign, SignOptions, verify } from 'jsonwebtoken';
 
 const PRIVATE_KEY = 'PRIVATE_KEY';
 
 interface TokenPayload {
-    exp: number;
-    accessTypes: string[];
-    name: string;
-    userId: number;
-  }
+  exp: number;
+  accessTypes: string[];
+  name: string;
+  userId: number;
+}
 
-export const validateToken = (token: string)  =>{
-   
-      return new Promise((resolve, reject) => {
-        verify(token, PRIVATE_KEY, (error, decoded) => {
-          if (error) return reject(error);
-    
-          resolve(decoded);
-        })
-      });
+export const validateToken = (token: string) => {
+
+  return new Promise((resolve, reject) => {
+    verify(token, PRIVATE_KEY, (error, decoded) => {
+      if (error) return reject(error);
+
+      resolve(decoded);
+    })
+  });
 }
 export const generateToken = async () => {
-    const payload = {
-        name: 'Usuário da API',
-        userId: 123,
-        accessTypes: [
-          'getExtrato'
-        ]
-      };
-      const signInOptions: SignOptions = {
-        // RS256 uses a public/private key pair. The API provides the private key
-        // to generate the JWT. The client gets a public key to validate the
-        // signature
-        algorithm: 'HS256',
-        expiresIn: '60000',// 1 min
-        allowInvalidAsymmetricKeyTypes: true,
-      };
-      // generate JWT
+  const payload = {
+    name: 'Usuário da API',
+    userId: 123,
+    accessTypes: [
+      'getExtrato'
+    ]
+  };
+  const signInOptions: SignOptions = {
+    // RS256 uses a public/private key pair. The API provides the private key
+    // to generate the JWT. The client gets a public key to validate the
+    // signature
+    algorithm: 'HS256',
+    expiresIn: '20000',// 20 seg
+    allowInvalidAsymmetricKeyTypes: true,
+  };
+  // generate JWT
   return sign(payload, PRIVATE_KEY, signInOptions);
-  }
+}
